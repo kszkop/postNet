a821e4aa553c57560fdba2b82078b6afa22686fa
Krzysztof Szkop <krzysztof@Krzysztofs-Laptop.local>
2023-11-05 11:16:36 +0000

    KS

--- R/contentAnalysis.R
+++ R/contentAnalysis.R
@@ -15,24 +15,8 @@ contentAnalysis <- function(annot,
                             plotType = "boxplot",
                             pdfName = NULL) {
   #
-  checkParameters(annot, ads, regulation, contrast, geneList, geneListcolours, customBg, selection, region, comparisons, plotOut, plotType)
-  
-  if(!isDNAsequence(contentIn)){
-    stop("'contentIn' must be a character vector with DNA sequences")
-  }
-  
-  if(!is.numeric(subregion) || !length(subregion)==1){
-    stop("'subregion' must be a numeric and just number")
-  }
-  
-  if (is.character(subregionSel) && length(subregionSel) == 1) {
-    if (!subregionSel %in% c("select", "exclude")) {
-      stop("'subregionSel' must be a character and only 'select' or 'exclude'")
-    }
-  } else {
-    stop("'subregionSel' must be a character and only 'select' or 'exclude'")
-  }
-  
+  checkParameters(annot, ads, regulation, contrast, geneList, geneListcolours, customBg, selection, region, comparisons, plotOut, plotType,contentIn,subregion,subregionSel)
+
   ####
   annotBg <- gSel(annot = annot, ads = ads, customBg = customBg, geneList = geneList)
   #
@@ -64,55 +48,20 @@ contentAnalysis <- function(annot,
       if (isTRUE(plotOut)) {
         #
         resOut <- resSel(vIn = contentOut, ads = ads, regulation = regulation, contrast = contrast, customBg = customBg, geneList = geneList)
-        coloursOut <- coloursSel(ads = ads, regulation = regulation, geneList = geneList, geneListcolours = geneListcolours, customBg = customBg)
+        if(length(resOut)==0){
+          stop('There are no regulated genes. Check the input or run without indicating regulation and comparisons')
+        }
+        coloursOut <- coloursSel(resOut=resOut, geneList = geneList, geneListcolours = geneListcolours)
+
         # Plot
         pdf(ifelse(is.null(pdfName), paste(reg, content, "Ncontent.pdf", sep = "_"), paste(pdfName, reg, content, "Ncontent.pdf", sep = "_")), width = 8, height = 8, useDingbats = F)
-        if (plotType == "boxplot" | plotType == "violin") {
-          par(mar = c(8, 12, 5, 4), bty = "l", font = 2, font.axis = 2, font.lab = 2, cex.axis = 1.4, cex.main = 1.7, cex.lab = 1.3)
-          # 
-          if (!is.null(regulation)) {
-            xlimIn <- c(0.5, length(regulation) + ifelse(!is.null(geneList), length(geneList), 0) + 1.5)
-          } else {
-            xlimIn <- c(0.5, length(geneList) + 1.5)
-          }
-          plot(1, 1, xlim = xlimIn, ylim = c(0, 100 + (length(comparisons) * 5)), xaxt = "n", xlab = "", ylab = "", type = "n", main = "", lwd = 1, bty = "n", yaxt = "n", font = 2, frame.plot = FALSE)
-
-          axis(side = 2, font = 2, las = 2, lwd = 2, at = seq(0, 100, 25), labels = seq(0, 100, 25))
-          text(1:length(resOut), par("usr")[3] - 0.45, labels = names(resOut), xpd = NA, cex = 0.9, srt = 45, adj = 1)
-          mtext(side = 2, line = 6, paste(reg, toupper(content), "content (%)", sep = " "), col = "black", font = 2, cex = 1.7, at = 50)
-          #
-          if (!is.null(ads) | !is.null(customBg)) {
-            abline(lty = 5, h = median(resOut[[1]]))
-          }
-          for (i in 1:length(resOut)) {
-            if (plotType == "violin") {
-              vioplot::vioplot(resOut[[i]], add = TRUE, at = i, col = coloursOut[i], xaxt = "n", xlab = "", ylab = "", main = "", lwd = 1, bty = "n", yaxt = "n", font = 2, frame.plot = FALSE)
-            } else if (plotType == "boxplot") {
-              boxplot(resOut[[i]], add = TRUE, at = i, col = coloursOut[i], xaxt = "n", xlab = "", ylab = "", type = "n", main = "", lwd = 1, bty = "n", yaxt = "n", font = 2, frame.plot = FALSE, outcol = "grey65", whiskcol = "grey65", outline = FALSE, medcol = "black", staplelty = 0, whisklty = 1)
-            }
-            text(i, 0, round(mean(resOut[[i]], 0)), font = 2)
-          }
+        
+        if (plotType == "boxplot"){
+          plotBoxplots(resOut, coloursOut, comparisons)
+        } else if(plotType == "violin") {
+          plotViolin(resOut, coloursOut, comparisons)
         } else if (plotType == "ecdf") {
-          #
-          xlim_min <- as.numeric(quantile(as.numeric(unlist(resOut)), 0.01))
-          xlim_max <- as.numeric(quantile(as.numeric(unlist(resOut)), 0.99))
-          #
-          par(mar = c(5, 5, 8, 4), bty = "l", font = 2, font.axis = 2, font.lab = 2, cex.axis = 1.4, cex.main = 1.7, cex.lab = 1.3)
-          plot(ecdf(resOut[[1]]), col = coloursOut[1], main = "", xlab = "", ylab = "", verticals = TRUE, do.p = FALSE, lwd = 3, bty = "n", yaxt = "none", font = 2, xlim = c(xlim_min, xlim_max), xaxt = "none")
-
-          for (i in 2:length(resOut)) {
-            lines(ecdf(resOut[[i]]), col = coloursOut[i], main = "", xlab = "", verticals = TRUE, do.p = FALSE, lwd = 4)
-          }
-
-          mtext(side = 1, line = 4, paste(paste(reg, toupper(content), "content (%)", sep = " "), "\n", reg, sep = ""), col = "black", font = 2, cex = 1.2)
-          mtext(side = 2, line = 3, "Fn(x)", col = "black", font = 2, cex = 1.2)
-
-          axis(side = 1, seq(floor(xlim_min), ceiling(xlim_max), 10), font = 2, lwd = 2)
-          axis(side = 2, seq(0, 1, 0.2), font = 2, las = 2, lwd = 2)
-        }
-        # Plot stats
-        if (!is.null(comparisons)) {
-          addStats(comparisons, ads, customBg, plotType, resOut, coloursOut)
+          plotEcdf(resOut, coloursOut, comparisons)
         }
         dev.off()
       }
@@ -123,18 +72,3 @@ contentAnalysis <- function(annot,
   return(contentFinal)
 }
 
-isDNAsequence <- function(contentIn) {
-  if (!is.character(contentIn)) {
-    return(FALSE)
-  }
-  
-  # Define a regular expression pattern for valid DNA characters
-  pattern <- "^[ACGTacgt]+$"
-  
-  # Use grepl to check if contentIn matches the pattern
-  if (all(grepl(pattern, contentIn))) {
-    return(TRUE)
-  } else {
-    return(FALSE)
-  }
-}
---
+++ R/qcchecks.R
@@ -0,0 +1,249 @@
+checkParameters <- function(annot, 
+                            ads, 
+                            regulation, 
+                            contrast, 
+                            geneList, 
+                            geneListcolours, 
+                            customBg, 
+                            selection, 
+                            region, 
+                            comparisons, 
+                            plotOut,
+                            plotType,
+                            contentIn=NULL,
+                            subregion=NULL,
+                            subregionSel=NULL,
+                            ){
+  ####
+  if(!is.null(ads) && !is.null(geneList)){
+    stop("please provide anota2seq object or genelist, not both.")
+  }
+  # Check for other input conditions as needed
+  checkAnnot(annot)
+  
+  region <- checkRegion(region)
+  
+  selection <- checkSelection(selection)
+  
+  if(!checkLogicalArgument(plotOut)){
+    stop("'plotOut' can only be only be logical: TRUE of FALSE ")
+  } 
+  if(isTRUE(plotOut)){
+    plotType <- checkPlotType(plotType)
+  }
+  if(!is.null(ads)){
+    if (!checkAds(ads)) {
+      stop("ads is not a valid 'Anota2seqDataSet' object.")
+    }
+    if (!is.null(regulation) && !is.character(regulation) && !regulation %in% c("translationUp","translationDown","translatedmRNAUp","translatedmRNADown","bufferingmRNAUp","bufferingmRNADown","mRNAAbundanceUp","mRNAAbundanceDown","totalmRNAUp","totalmRNADown")) {
+      stop("'regulation' should be a character vector chosen from translationUp,translationDown,translatedmRNAUp,translatedmRNADown,bufferingmRNAUp,bufferingmRNADown,mRNAAbundanceUp,mRNAAbundanceDown,totalmRNAUp,totalmRNADown")
+    }
+    if (!is.null(regulation)){
+      if(!is.null(contrast) && !is.numeric(contrast) && !length(contrast) == length(regulation) && !contrast %in% seq(1,ncol(ads@contrasts),1)){
+        stop("'contrast' should be a numeric vector chosen from each regulation mode")
+      }
+    }
+  } 
+  if(is.null(ads)){
+    if(is.null(geneList)){
+      stop('Either anota2seq object of gene list must be provided')
+    } else {
+      if(!checkGeneList(geneList)){
+        stop("'geneList' is empty or not named")
+      }
+      if (!is.null(geneListcolours) && !is.character(geneListcolours) && !length(geneListcolours)== length(geneList)) {
+        stop("'geneListcolours' should be a character vector of the same length as geneList.")
+      }
+    }
+  }
+  if(!is.null(customBg)){
+    if(!is.character(customBg)){
+      stop("'customBg' is not character vector")
+    }
+    if(!length(setdiff(unlist(geneList), customBg)==0)){
+      stop("There are entries in geneList that are not in 'customBg'")
+    }
+  }
+  if(!is.null(comparisons)){
+    if(!checkComparisons(comparisons)){
+      stop("'comparisons' must be a list of numeric vector for paired comparisons example: list(c(0,2),c(0,1)). 0 is always a background.")
+    }
+    if(length(which(unique(unlist(list(c(0,2),c(0,1))))==0)>0) && is.null(customBg) && is.null(ads)){
+      stop(" 0 is always a background, but no background provided")
+    }
+  }
+  if(!isDNAsequence(contentIn)){
+    stop("'contentIn' must be a character vector with DNA sequences")
+  }
+  
+  if(!is.numeric(subregion) || !length(subregion)==1){
+    stop("'subregion' must be a numeric and just number")
+  }
+  
+  if (is.character(subregionSel) && length(subregionSel) == 1) {
+    if (!subregionSel %in% c("select", "exclude")) {
+      stop("'subregionSel' must be a character and only 'select' or 'exclude'")
+    }
+  } else {
+    stop("'subregionSel' must be a character and only 'select' or 'exclude'")
+  }
+}
+
+checkRegion <- function(region, convertToUppercase = TRUE) {
+  valid_regions <- c('UTR3', 'CDS', 'UTR5')
+  
+  if (is.null(region) || !is.character(region) || length(region) == 0) {
+    stop("'region' must be a non-empty character vector with valid values, to choose from 'UTR3', 'CDS', 'UTR5'.")
+  }
+  
+  if (convertToUppercase) {
+    region <- toupper(region)
+  }
+  
+  if (!all(region %in% valid_regions)) {
+    stop("'region' must contain valid values: 'UTR3', 'CDS', 'UTR5'.")
+  }
+  return(region)
+}
+
+checkSelection <- function(selection, convertToLowercase = TRUE) {
+  valid_selection <- c('random', 'longest', 'shortest')
+  
+  if (is.null(selection) || !is.character(selection) || length(selection) == 0) {
+    stop("'selection' must be one of: 'random', 'longest', or 'shortest'.")
+  }
+  
+  if (convertToLowercase) {
+    selection <- tolower(selection)
+  }
+  
+  if (!selection %in% tolower(valid_selection)) {
+    stop("'selection' must be one of: 'random', 'longest', or 'shortest'.")
+  }
+  
+  return(selection)
+}
+
+checkPlotType <- function(plotType, convertToLowercase = TRUE) {
+  valid_plottypes <- c('boxplot', 'violin', 'ecdf')
+  
+  if (is.null(plotType) || !is.character(plotType) || length(plotType) == 0) {
+    stop("'plotType' must be one of: 'boxplot', 'violin', or 'ecdf'.")
+  }
+  
+  if (convertToLowercase) {
+    plotType <- tolower(plotType)
+  }
+  
+  if (!plotType %in% tolower(valid_plottypes)) {
+    stop("'plotType' must be one of: 'boxplot', 'violin', or 'ecdf'.")
+  }
+  
+  return(plotType)
+}
+
+checkAnnot <- function(annot, expectedCols = c("id", "geneID", "UTR5_seq", "CDS_seq", "UTR3_seq")) {
+  if (is.null(annot) || !is.data.frame(annot)) {
+    stop("'annot' should be a data frame.")
+  }
+  
+  if (!all(expectedCols %in% colnames(annot))) {
+    stop("The following columns are missing in 'annot': ", paste(expectedCols[!expectedCols %in% colnames(annot)], collapse = ", "))
+  }
+}
+
+# Check if 'ads' is a valid 'Anota2seqDataSet' object
+checkAds <- function(obj) {
+  if (!inherits(obj, "Anota2seqDataSet")) {
+    return(FALSE)
+  }
+  return(TRUE)
+}
+
+checkGeneList <- function(obj) {
+  if (is.list(obj) && length(obj) > 0 && any(names(obj) != "")) {
+    return(TRUE)
+  }
+  return(FALSE)
+}
+
+checkComparisons <- function(obj) {
+  if (!is.list(obj)) {
+    return(FALSE)
+  }
+  
+  all(sapply(obj, function(x) is.numeric(x) && length(x) == 2))
+}
+
+checkLogicalArgument <- function(arg) {
+  if (is.logical(arg) && length(arg) == 1) {
+    return(TRUE)
+  } else {
+    return(FALSE)
+  }
+}
+
+# Function to validate the source input
+checkSource <- function(source) {
+  valid_sources <- c("create", "createFromSourceFiles", "load", "custom", "createFromFiles")
+  if (!(source %in% valid_sources)) {
+    stop("Invalid source. Please provide a valid source option.")
+  }
+}
+
+# Function to validate the species input
+checkSpecies <- function(source, species) {
+  if (source %in% c("create", "load") && is.null(species)) {
+    stop("Please specify a species (e.g., 'human' or 'mouse').")
+  }
+  if (source %in% c("create", "load") && !(species %in% c("human", "mouse"))) {
+    stop("This option is only available for species: human and mouse at the moment. Please use option createFromFile.")
+  }
+}
+
+isDNAsequence <- function(contentIn) {
+  if (!is.character(contentIn)) {
+    return(FALSE)
+  }
+  
+  # Define a regular expression pattern for valid DNA characters
+  pattern <- "^[ACGTacgt]+$"
+  
+  # Use grepl to check if contentIn matches the pattern
+  if (all(grepl(pattern, contentIn))) {
+    return(TRUE)
+  } else {
+    return(FALSE)
+  }
+}
+
+# Function to validate specific input parameters
+checkInput <- function(source, customFile, rna_gbff_file, rna_fa_file, genomic_gff_file, posFile) {
+  if (source == "createFromSourceFiles") {
+    if (is.null(rna_gbff_file)) {
+      stop("Please provide an rna_gbff_file.")
+    }
+    if (is.null(rna_fa_file)) {
+      stop("Please provide an rna_fa_file.")
+    }
+    if (is.null(genomic_gff_file)) {
+      stop("Please provide a genomic_gff_file.")
+    }
+  } else if (source == "custom") {
+    if (is.null(customFile)) {
+      stop("Please provide a customFile.")
+    }
+  } else if (source == "createFromFiles") {
+    if (is.null(posFile)) {
+      stop("Please provide a posFile in the format: id, UTR5_len, CDS_stop, Total_len.")
+    }
+  }
+}
+
+# Function to download and unzip files
+downloadAndUnzip <- function(source, species) {
+  url <- generateDownloadURL(species)
+  download_and_unzip("customFasta.fa.gz", paste0(url, "_rna.fna.gz"))
+  download_and_unzip("customAnnot.gbff.gz", paste0(url, "_rna.gbff.gz"))
+  download_and_unzip("GeneRef.gff.gz", paste0(url, "_genomic.gff.gz"))
+}
