b1ecdab091cf73590b66a113d4ba0dcbdc2de780
Krzysztof Szkop <krzysztof@Krzysztofs-Laptop.local>
2023-11-09 11:56:24 +0000

    KS

--- R/qcchecks.R
+++ R/qcchecks.R
@@ -99,7 +99,7 @@ checkSource <- function(source) {
 # Function to validate the source input
 checkSourceFE <- function(sourceFE) {
   valid_sourcesFE <- c("create", "load", "custom")
-  if (!(sourceFE %in% valid_sources)) {
+  if (!(sourceFE %in% valid_sourcesFE)) {
     stop("Invalid sourceFE. Please provide a valid sourceFE option.")
   }
 }
