c7d09f566d375296f9e1b511643e4960e99ea218
Krzysztof Szkop <krzysztof@Krzysztofs-Laptop.local>
2024-07-12 11:26:04 +0100

    KS

--- R/uorf_analysis.R
+++ R/uorf_analysis.R
@@ -68,52 +68,52 @@ uorf_analysis <- function(a2sU,
   #  uorfOut <- sapply(seqTmp, function(x) calc_uORF(x, ext=NULL, context = context, unit = tolower(unitOut)), USE.NAMES=FALSE)
   #}
   #
-  names(uorfOut) <- names(seqTmp)
+  #names(uorfOut) <- names(seqTmp)
   #
-  if (tolower(unitOut) == "number" & isTRUE(plotOut)) {
-    #
-    resOut <- resQuant(qvec = uorfOut, a2sU = a2sU)
-    
-    if(length(resOut)==0){
-      stop('There are no regulated genes. Check the input or run without indicating regulation and comparisons')
-    }
-    colOut <- colPlot(a2sU)
-    #
-    resProp <- as.numeric()
-    for (i in 1:length(resOut)) {
-      resProp[i] <- length(resOut[[i]][resOut[[i]] > 0]) / length(resOut[[i]])
-    }
-    # Plot
-    pdf(ifelse(is.null(pdfName), paste("uORFs_", KozakContext, ".pdf", sep = ""), paste(pdfName, "_uORFs_", KozakContext, ".pdf", sep = "")), width = 8, height = 8, useDingbats = F)
-    par(mar = c(8, 12, 5, 4), bty = "l", font = 2, font.axis = 2, font.lab = 2, cex.axis = 1.4, cex.main = 1.7, cex.lab = 1.3)
-    barplot(resProp_any, col = colOut, xaxt = "n", xlab = "", ylab = "", main = "", lwd = 1, bty = "n", yaxt = "n", font = 2, ylim = c(0, ifelse(is.null(comparisons), 1, 1 + (length(comparisons) * 0.1))), space = 0)
-    barplot(resProp_strong, add=T, col = colorspace::darken(colOut,0.25),xaxt = "n", xlab = "", ylab = "", main = "", lwd = 1, bty = "n", yaxt = "n", ylim = c(0, ifelse(is.null(comparisons), 1, 1 + (length(comparisons) * 0.1))), space = 0)
-    
-    axis(side = 2, font = 2, las = 2, lwd = 2, at = seq(0, 1, 0.2), labels = seq(0, 1, 0.2))
-    text(seq(0.5, length(resOut), 1), par("usr")[3] - 0.05, labels = names(resOut), xpd = NA, cex = 0.9, srt = 45, adj = 1.1)
-
-    mtext(side = 2, line = 6, paste("proportion of uORFs in \n", KozakContext, "Kozak context", sep = ""), col = "black", font = 2, cex = 1.7, at = 0.5)
-
-    # Plot stats
-    if (!is.null(comparisons)) {
-      for (j in 1:length(comparisons)) {
-        if (names(resOut)[1] == 'background') {
-          compTmp <- comparisons[[j]] + 1
-        } else {
-          compTmp <- comparisons[[j]]
-        }
-        # stats
-        pvalTmp <- format(as.numeric(wilcox.test(resOut[[compTmp[1]]], resOut[[compTmp[2]]], alternative = "two.sided")[3]), scientific = TRUE, digits = 2)
-        #
-        yposTmp <- (max(as.numeric(resProp)) + 0.05) + (j * 0.1)
-        rect(xleft = compTmp[1] - 0.5, xright = compTmp[2] - 0.5, ybottom = yposTmp, ytop = yposTmp, lwd = 2)
-        #
-        text((sum(compTmp) / 2) - 0.5, yposTmp + 0.05, pvalTmp, cex = 0.75)
-      }
-    }
-    dev.off()
-  }
-  uORFFinal[[paste('uORFs',startCodon,KozakContext,sep='_')]] <- uorfOut
+  #if (tolower(unitOut) == "number" & isTRUE(plotOut)) {
+  #  #
+  #  resOut <- resQuant(qvec = uorfOut, a2sU = a2sU)
+  #  
+  #  if(length(resOut)==0){
+  #    stop('There are no regulated genes. Check the input or run without indicating regulation and comparisons')
+  #  }
+  #  colOut <- colPlot(a2sU)
+  #  #
+  #  resProp <- as.numeric()
+  #  for (i in 1:length(resOut)) {
+  #    resProp[i] <- length(resOut[[i]][resOut[[i]] > 0]) / length(resOut[[i]])
+  #  }
+  #  # Plot
+  #  pdf(ifelse(is.null(pdfName), paste("uORFs_", KozakContext, ".pdf", sep = ""), paste(pdfName, "_uORFs_", KozakContext, ".pdf", sep = "")), width = 8, height = 8, useDingbats = F)
+  #  par(mar = c(8, 12, 5, 4), bty = "l", font = 2, font.axis = 2, font.lab = 2, cex.axis = 1.4, cex.main = 1.7, cex.lab = 1.3)
+  #  barplot(resProp_any, col = colOut, xaxt = "n", xlab = "", ylab = "", main = "", lwd = 1, bty = "n", yaxt = "n", font = 2, ylim = c(0, ifelse(is.null(comparisons), 1, 1 + (length(comparisons) * 0.1))), space = 0)
+  #  barplot(resProp_strong, add=T, col = colorspace::darken(colOut,0.25),xaxt = "n", xlab = "", ylab = "", main = "", lwd = 1, bty = "n", yaxt = "n", ylim = c(0, ifelse(is.null(comparisons), 1, 1 + (length(comparisons) * 0.1))), space = 0)
+  #  
+  #  axis(side = 2, font = 2, las = 2, lwd = 2, at = seq(0, 1, 0.2), labels = seq(0, 1, 0.2))
+  #  text(seq(0.5, length(resOut), 1), par("usr")[3] - 0.05, labels = names(resOut), xpd = NA, cex = 0.9, srt = 45, adj = 1.1)
   #
-  return(uORFFinal)
+  #  mtext(side = 2, line = 6, paste("proportion of uORFs in \n", KozakContext, "Kozak context", sep = ""), col = "black", font = 2, cex = 1.7, at = 0.5)
+  #
+  #  # Plot stats
+  #  if (!is.null(comparisons)) {
+  #    for (j in 1:length(comparisons)) {
+  #      if (names(resOut)[1] == 'background') {
+  #        compTmp <- comparisons[[j]] + 1
+  #      } else {
+  #        compTmp <- comparisons[[j]]
+  #      }
+  #      # stats
+  #      pvalTmp <- format(as.numeric(wilcox.test(resOut[[compTmp[1]]], resOut[[compTmp[2]]], alternative = "two.sided")[3]), scientific = TRUE, digits = 2)
+  #      #
+  #      yposTmp <- (max(as.numeric(resProp)) + 0.05) + (j * 0.1)
+  #      rect(xleft = compTmp[1] - 0.5, xright = compTmp[2] - 0.5, ybottom = yposTmp, ytop = yposTmp, lwd = 2)
+  #      #
+  #      text((sum(compTmp) / 2) - 0.5, yposTmp + 0.05, pvalTmp, cex = 0.75)
+  #    }
+  #  }
+  #  dev.off()
+  #}
+  #uORFFinal[[paste('uORFs',startCodon,KozakContext,sep='_')]] <- uorfOut
+  #
+  #return(uORFFinal)
 }
