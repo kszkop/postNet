3d73cc58977c17a06def4e645092a21aba2de4a3
Kathleen Watt <kathleen.watt@ki.se>
2025-01-29 13:25:17 +0000

    KW

--- R/qcchecks.R
+++ R/qcchecks.R
@@ -27,7 +27,7 @@ check_adjObj <- function(adjObj) {
     if (!is.character(entry) || !all(nchar(entry) > 0)) {
       stop("The entries in the adjObj list should be character vectors with DNA nucleotide sequences.")
     }
-    if (!check_DNAsequence(entry[1])) {
+    if (!check_DNAsequence(entry)) {
       stop("It looks like the sequences provided in adjObj are not DNA sequences.")
     }
   }
