8559c9d835d5ea619fb5b4e6a110e841f6a8193d
Krzysztof Szkop <krzysztof@Krzysztofs-Laptop.local>
2025-02-07 18:16:57 +0000

    KS

--- R/checkAvailableVersions.R
+++ R/checkAvailableVersions.R
@@ -1,3 +1,19 @@
+#' Check the available RefSeq release versions for pre-prepared reference sequence annotations
+#'
+#' The \code{checkAvailableVersions} function retrieves and lists the available release versions for the pre-prepared reference sequence
+#' annotations based on the NCBI RefSeq database.
+#'
+#' @usage
+#' checkAvailableVersions(
+#'   species = NULL,
+#' )
+#'
+#' @param species Select the species of interest. Currently, \code{'human'} and \code{'mouse'} are supported.
+#'
+#' @seealso
+#' \code{\link[postNet]{postNetStart}} for details on creating reference sequence annotations.
+#'
+#' @export
 
 checkAvailableVersions <- function(species) {
   # Define the base directory for annotation files
