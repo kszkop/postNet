2fe979d658bee75ccd2e836e7c4b430b4f812c69
kwattever <kathleen.watt@ki.se>
2025-04-03 00:42:02 +0200

    KW

--- R/goDotplot.R
+++ R/goDotplot.R
@@ -59,13 +59,33 @@ goDotplot <- function(ptn,
       #
       colOut <- colPlot(ptn)[-1]
       names(colOut) <- names(ptn@dataIn@geneList)
-      
+      #
+      if (all(goDf$scale == floor(goDf$scale))) {
+        # Integer scale values
+        size_breaks <- pretty(range(goDf$scale, na.rm = TRUE), n = 4)
+        size_breaks <- size_breaks[size_breaks == floor(size_breaks)]
+        size_limits <- range(size_breaks, na.rm = TRUE)
+      } else {
+        # Decimal scale values between 0 and 1
+        rng <- range(goDf$scale, na.rm = TRUE)
+        step <- 0.05  # finer step for better resolution in small ranges
+        
+        min_val <- floor(rng[1] / step) * step
+        max_val <- ceiling(rng[2] / step) * step
+        
+        size_breaks <- seq(min_val, max_val, by = step)
+        size_limits <- c(min_val, max_val)
+      }
+      #
       pdf(nameOut, width = 8, height = 8, useDingbats = F)
       par(mar = c(5, 5, 3, 3), bty = "l", font = 2, font.axis = 2, font.lab = 2, cex.axis = 1.3, cex.main = 1.7, cex.lab = 1)
       pOut <- ggplot2::ggplot(goDf, ggplot2::aes(x=log10fdr, y=reorder(Description,log10fdr), size=scale,colour = regulation)) +
         ggplot2::geom_point() +
         ggplot2::scale_color_manual(values = colOut) +  
-        ggplot2::scale_size_continuous(name = size) + 
+        ggplot2::scale_size_continuous(name = size,
+                                       breaks = size_breaks,
+                                       limits = size_limits,
+                                       range = c(2, 8)) + 
         ggplot2::theme_bw() +
         ggplot2::theme(panel.grid.major = ggplot2::element_line(linetype = 'dashed', linewidth = 0.25), panel.grid.minor = ggplot2::element_blank(),panel.background = ggplot2::element_blank(), legend.key.size = ggplot2::unit(0.5, 'cm')) +   
         ggplot2::xlab('-log10 FDR') +
@@ -107,6 +127,24 @@ goDotplot <- function(ptn,
           colOut <- colPlot(ptn)[-1]
           names(colOut) <- names(ptn_geneList(ptn))
           #
+          # Determine breaks and limits for the size scale
+          if (all(goDf$scale == floor(goDf$scale))) {
+            # Integer scale values
+            size_breaks <- pretty(range(goDf$scale, na.rm = TRUE), n = 4)
+            size_breaks <- size_breaks[size_breaks == floor(size_breaks)]
+            size_limits <- range(size_breaks, na.rm = TRUE)
+          } else {
+            # Decimal scale values between 0 and 1
+            rng <- range(goDf$scale, na.rm = TRUE)
+            step <- 0.05  # finer step for better resolution in small ranges
+            
+            min_val <- floor(rng[1] / step) * step
+            max_val <- ceiling(rng[2] / step) * step
+            
+            size_breaks <- seq(min_val, max_val, by = step)
+            size_limits <- c(min_val, max_val)
+          }
+          #
           pdf(nameOut, width = 8, height = 8, useDingbats = F)
           par(mar = c(5, 5, 3, 3), bty = "l", font = 2, font.axis = 2, font.lab = 2, cex.axis = 1.3, cex.main = 1.7, cex.lab = 1)
           pOut <- ggplot2::ggplot(goDf, ggplot2::aes(x=log10fdr, y=reorder(Description,log10fdr), size=scale)) +
@@ -115,7 +153,10 @@ goDotplot <- function(ptn,
             ggplot2::theme_bw() +
             ggplot2::theme(panel.grid.major = ggplot2::element_line(linetype = 'dashed', linewidth = 0.25), panel.grid.minor = ggplot2::element_blank(),panel.background = ggplot2::element_blank(), legend.key.size = ggplot2::unit(0.5, 'cm')) +   
             ggplot2::xlab('-log10 FDR') +
-            ggplot2::scale_size_continuous(name = size) + 
+            ggplot2::scale_size_continuous(name = size,
+                                           breaks = size_breaks,
+                                           limits = size_limits,
+                                           range = c(2, 8)) + 
             ggplot2::ylab(" ") +
             ggplot2::ggtitle(paste(names(goIn)[i],sel, sep='_')) 
           print(pOut)
